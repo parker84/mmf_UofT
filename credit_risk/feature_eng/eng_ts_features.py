bce9c24036fea68aa90c92e0e5f3beb58dc4fa4f
bryparker <brydon.parker@mail.utoronto.ca>
2018-11-11 15:44:18 -0500

    starting a script w some time series feature ideas

---
+++ credit_risk/feature_eng/eng_ts_features.py
@@ -0,0 +1,18 @@
+
+
+
+class EngTsFeats(object):
+
+    # TODO: do this
+    def __init__(self):
+        pass
+
+    def slope_on_cc_spending(self):
+        pass
+
+    def forecast_cc_spending_curr_app(self):
+        pass
+
+    def forecast_num_late_payments_curr_app(self):
+        pass
+
